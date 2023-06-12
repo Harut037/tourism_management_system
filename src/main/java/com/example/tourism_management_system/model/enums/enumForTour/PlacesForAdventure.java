ef2cbf92fa1b59f43f0669269d18cd935e6abb0c
Mariam2905 <avetisyanmariam1990@gmail.com>
2023-06-12 14:52:20 +0400

    Update enums

--- src/main/java/com/example/tourism_management_system/model/enums/enumForTour/PlacesForAdventure.java
+++ src/main/java/com/example/tourism_management_system/model/enums/enumForTour/PlacesForAdventure.java
@@ -1,6 +1,30 @@
 package com.example.tourism_management_system.model.enums.enumForTour;
 
 public enum PlacesForAdventure {
-    LASTIVER,
-    RAFTING;
+    LASTIVER (9,280,17500),
+    RAFTING (400,10,25000);
+
+    private int duration;
+    private int distance;
+    private int cost;
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    PlacesForAdventure(int distance, int duration, int cost) {
+        if (distance >= 7 && duration > 0 && cost >= 0) {
+            this.distance = distance;
+            this.duration = duration;
+            this.cost = cost;
+        } else throw new IllegalArgumentException();
+    }
 }
--- src/main/java/com/example/tourism_management_system/model/enums/enumForTour/PlacesForCampaign.java
+++ src/main/java/com/example/tourism_management_system/model/enums/enumForTour/PlacesForCampaign.java
@@ -1,7 +1,31 @@
 package com.example.tourism_management_system.model.enums.enumForTour;
 
 public enum PlacesForCampaign {
-    DIMAC,
-    HATIS,
-    AGHMAGHAN;
+    DIMAC (220,12,10000),
+    HATIS (100,8,7000),
+    AGHMAGHAN(275,11,8000);
+
+    private int duration;
+    private int distance;
+    private int cost;
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    PlacesForCampaign(int distance, int duration, int cost) {
+        if (distance >= 7 && duration > 0 && cost >= 0) {
+            this.distance = distance;
+            this.duration = duration;
+            this.cost = cost;
+        } else throw new IllegalArgumentException();
+    }
 }
